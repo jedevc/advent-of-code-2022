8739246aa2ce1ac6117290ecdbc1d3638a6f2524
Justin Chadwell <me@jedevc.com>
2022-12-01 12:35:14 +0000

    Day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,14 +1,30 @@
 use advent_of_code_2022::{main, Solver};
 
-struct Day1 {}
+struct Day1 {
+    calories: Vec<Vec<i64>>,
+}
 
 impl Solver<i64> for Day1 {
-    fn new(_problem: &str) -> Self {
-        Day1 {}
+    fn new(problem: &str) -> Self {
+        let calories = problem
+            .split("\n\n")
+            .map(|elf| elf.split("\n").map(|s| s.parse().unwrap()).collect())
+            .collect();
+        Day1 { calories: calories }
     }
 
     fn solve(&self) -> (Option<i64>, Option<i64>) {
-        (None, None)
+        let mut counts: Vec<i64> = self
+            .calories
+            .iter()
+            .map(|calories| calories.iter().sum())
+            .collect();
+        counts.sort();
+        counts.reverse();
+
+        let part1 = counts[0];
+        let part2 = counts[..3].iter().sum();
+        (Some(part1), Some(part2))
     }
 }
 
@@ -17,7 +33,7 @@ mod day1tests {
     use super::*;
     use advent_of_code_2022::test_example;
 
-    test_example!(Day1, "day1.txt", (None, None));
+    test_example!(Day1, "day1.example.txt", (Some(24000), Some(45000)));
 }
 
 main!(Day1, "day1.txt");
